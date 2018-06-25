0a99e8085c6a80b10689f144ccdb50af35627f40
vamsik <vamsik@PC02709.hmsl.local>
2018-06-25 09:04:02 +0100

    Added implementation for findByFirstName

--- accounts/src/main/java/com/qa/consulting/accounts/repository/AccountRepository.java
+++ accounts/src/main/java/com/qa/consulting/accounts/repository/AccountRepository.java
@@ -1,9 +1,11 @@
 package com.qa.consulting.accounts.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.qa.consulting.accounts.model.Account;
 
 public interface AccountRepository extends JpaRepository<Account, Integer> {
-
+	public Optional<Account> findByFirstName(String firstName);
 }
