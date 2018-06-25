2154c52dafc3f057e04268c4e556cf5b916dd134
vamsik <vamsik@PC02709.hmsl.local>
2018-06-25 09:25:25 +0100

    Passing Tests - After implementing Account Service

---
+++ accounts/src/main/java/com/qa/consulting/accounts/service/AccountServiceImpl.java
@@ -0,0 +1,46 @@
+package com.qa.consulting.accounts.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.qa.consulting.accounts.model.Account;
+import com.qa.consulting.accounts.repository.AccountRepository;
+
+@Service
+public class AccountServiceImpl implements AccountService {
+
+	@Autowired
+	AccountRepository accountRepository;
+
+	@Override
+	public List<Account> getAllAccounts() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Optional<Account> getAccountById(int id) {
+		return accountRepository.findById(id);
+	}
+
+	@Override
+	public Optional<Account> getAccountByFirstName(String firstName) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Account save(Account account) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void deleteAccount(int id) {
+		// TODO Auto-generated method stub
+
+	}
+}
