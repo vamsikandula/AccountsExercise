6d0ab295d2e832622ccbce96e16ba0128bc832b4
vamsik <vamsik@PC02709.hmsl.local>
2018-06-25 09:00:33 +0100

    Failure test - find account with a given first name.

--- accounts/src/test/java/com/qa/consulting/accounts/repository/AccountRepositoryTest.java
+++ accounts/src/test/java/com/qa/consulting/accounts/repository/AccountRepositoryTest.java
@@ -13,45 +13,49 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.test.context.junit4.SpringRunner;
 
-
 import com.qa.consulting.accounts.model.Account;
 
 @RunWith(SpringRunner.class)
 @DataJpaTest
 public class AccountRepositoryTest {
 
-
 	private final String FIRST_NAME = "Jon";
-	
-	// loading 3 accounts via data.sql 
-	
-    @Autowired
-    private TestEntityManager entityManager;
-	
+
+	// loading 3 accounts via data.sql
+
+	@Autowired
+	private TestEntityManager entityManager;
+
 	@Autowired
 	private AccountRepository accountRepository;
-	
+
 	@Test
 	public void whenFindAll_thenReturnAllAccounts() {
 		List<Account> accounts = accountRepository.findAll();
-		assertEquals(3,accounts.size());
+		assertEquals(3, accounts.size());
 	}
-	
-	 @Test
-	    public void whenFindById_thenReturnAccount() {
-	        Account account = new Account("Tom","Bew", 7676);
-	        entityManager.persistAndFlush(account);
 
-	        Optional<Account> accountfromDb = accountRepository.findById(account.getId());	        
-	        assertThat(accountfromDb.get().getFirstName()).isEqualTo(account.getFirstName());
+	@Test
+	public void whenFindById_thenReturnAccount() {
+		Account account = new Account("Tom", "Bew", 7676);
+		entityManager.persistAndFlush(account);
+
+		Optional<Account> accountfromDb = accountRepository.findById(account.getId());
+		assertThat(accountfromDb.get().getFirstName()).isEqualTo(account.getFirstName());
 	}
-	 
-	 @Test
-	 public void whenInvalidId_thenReturnOptionalNotPresent() {   	
-	 	
-	 	Optional<Account> accountfromDb = accountRepository.findById(-11);
-	 	assertThat(accountfromDb.isPresent()).isFalse();
-	 }
- 
+
+	@Test
+	public void whenInvalidId_thenReturnOptionalNotPresent() {
+
+		Optional<Account> accountfromDb = accountRepository.findById(-11);
+		assertThat(accountfromDb.isPresent()).isFalse();
+	}
+
+	@Test
+	public void givenFirstNamePresentInDB_WhenFindByFirstName_ThenReturnAccount() {
+		Optional<Account> account = accountRepository.findByFirstName(FIRST_NAME);
+		assertThat(account.isPresent()).isEqualTo(true);
+		assertThat(account.get().getFirstName()).isEqualTo(FIRST_NAME);
+	}
 
 }
