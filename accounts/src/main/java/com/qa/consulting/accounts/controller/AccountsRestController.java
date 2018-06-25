f2557f0586d1266d0a3d927ccf7667e3fa659e42
vamsik <vamsik@PC02709.hmsl.local>
2018-06-25 09:52:52 +0100

    Passing Test - account controller POST

--- accounts/src/main/java/com/qa/consulting/accounts/controller/AccountsRestController.java
+++ accounts/src/main/java/com/qa/consulting/accounts/controller/AccountsRestController.java
@@ -1,11 +1,18 @@
 package com.qa.consulting.accounts.controller;
 
+import java.net.URI;
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.qa.consulting.accounts.model.Account;
 import com.qa.consulting.accounts.service.AccountService;
@@ -21,5 +28,20 @@ public class AccountsRestController {
     public List<Account> getAllAccount() {
         return accountService.getAllAccounts();
     }
+    
+    
+    @PostMapping("/accounts")
+    public ResponseEntity<Object> createAccount(@Valid @RequestBody Account account) {
+        
+        Account saved = accountService.save(account);        
+        URI location = ServletUriComponentsBuilder
+        		.fromCurrentRequest()
+        		.path("/{id}")
+        		.buildAndExpand(saved.getId()).toUri();
+        
+        return ResponseEntity.created(location).body("Account has been successfully added");
+        
+    }
+    
 
 }
