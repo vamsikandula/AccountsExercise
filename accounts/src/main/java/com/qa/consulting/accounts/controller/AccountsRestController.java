d4478ff51dac7bdb99c1dfa5bd42562385fa2ecb
vamsik <vamsik@PC02709.hmsl.local>
2018-06-25 09:48:27 +0100

    Passing Test - AccountController, find all accounts

--- accounts/src/main/java/com/qa/consulting/accounts/controller/AccountsRestController.java
+++ accounts/src/main/java/com/qa/consulting/accounts/controller/AccountsRestController.java
@@ -1,5 +1,25 @@
 package com.qa.consulting.accounts.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.qa.consulting.accounts.model.Account;
+import com.qa.consulting.accounts.service.AccountService;
+
+@RestController
+@RequestMapping("/account-project/rest")
 public class AccountsRestController {
+	
+	@Autowired
+	private AccountService accountService;
+
+    @GetMapping("/accounts")
+    public List<Account> getAllAccount() {
+        return accountService.getAllAccounts();
+    }
 
 }
